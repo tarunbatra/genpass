3dc35477034d85bfab917d499e8b3aa65fd6275a
Conor Hanrahan <conor.hanrahan@okta.com>
2022-09-13 15:06:53 -0700

    Fixed file path issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import PasswordGenerator from './PasswordGenerator'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export { PasswordGenerator };
