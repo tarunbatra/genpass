3dad22c473597de78ebfcf1fc8b4daabcb4ceaad
Tarun Batra <tarun.batra@okta.com>
2022-09-13 17:36:37 -0700

    Add tests for library

--- src/lib/genpass.test.js
+++ src/lib/genpass.test.js
@@ -1,3 +1,4 @@
+import { TYPE } from './constants'
 import genPass from './genpass'
 
 describe('lib', () => {
@@ -26,6 +27,27 @@ describe('lib', () => {
     it('returns a passphrase with symbols', () => {
       const password = genPass({ symbols: true })
       expect(password).toEqual(expect.stringMatching(/[@!$]+/))
+    })
+  })
+
+  describe('used with type=random', () => {
+    it('returns a random string', () => {
+      const password1 = genPass({ type: TYPE.RANDOM })
+      const password2 = genPass({ type: TYPE.RANDOM })
+      expect(password1).not.toEqual(password2)
+    })
+  })
+  describe('used with type=pin', () => {
+    it('returns a random 4 digit pin', () => {
+      const password = genPass({ type: TYPE.PIN })
+      expect(password).toEqual(expect.stringMatching(/^\d{4}$/))
+    })
+  })
+
+  describe('used with type=pin and length=16', () => {
+    it('returns a random 16-digit pin', () => {
+      const password = genPass({ type: TYPE.PIN, length: 16 })
+      expect(password).toEqual(expect.stringMatching(/^\d{16}$/))
     })
   })
 })
