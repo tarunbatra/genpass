6195a59d1f77e623b982c35bc14018e780d7823c
Tarun Batra <tarun.batra@okta.com>
2022-09-15 10:49:38 -0700

    Updates

---
+++ precache-manifest.836ad788836e9e2dd14c77c02822aea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f958f413c0d83c969bdf0e165a6659aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9c1543e430f3b80dc3a",
+    "url": "./static/css/main.fb654908.chunk.css"
+  },
+  {
+    "revision": "7ba18c6e6485c3ade4ea",
+    "url": "./static/js/2.4b65cf3f.chunk.js"
+  },
+  {
+    "revision": "c72d742dc4207424ffabe5f07e33e0b5",
+    "url": "./static/js/2.4b65cf3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c1543e430f3b80dc3a",
+    "url": "./static/js/main.3b740562.chunk.js"
+  },
+  {
+    "revision": "64cbdd37499091208c1d",
+    "url": "./static/js/runtime-main.4770cef6.js"
+  },
+  {
+    "revision": "e39bce750ca5dff5b7a010f457023865",
+    "url": "./static/media/logo.e39bce75.svg"
+  }
+]);
