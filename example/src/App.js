f0f3085c29a9a6e9fa7f24884d47a5d844852358
Joel Glovier <joel.glovier@okta.com>
2022-09-15 13:48:51 -0700

    more style tweaks
    - use link color on info link
    - add muscle emoji
    - heading styles to match design comps

--- example/src/App.js
+++ example/src/App.js
@@ -13,9 +13,9 @@ const App = () => {
         <section className="copy">
           <img id="logo" src={logo} alt="Okta" />
           <h2>Password generator</h2>
-          <h3>Generate a strong login password. &#x2728;&#x1F512;&#x2728;</h3>
+          <h3>Generate a strong login password. &#x2728;&#x1f4aa;&#x1F512;&#x2728;</h3>
           <p>
-            This password is not saved or seen on our servers.
+            This password is not saved or seen on our servers.<br/>
             Use it for any account login &ndash; Okta or anywhere else!
           </p>
           <p className="info-link">
