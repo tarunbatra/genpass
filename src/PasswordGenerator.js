8c426f48be10ae26994e79666c182b8032d88059
Conor Hanrahan <conor.hanrahan@okta.com>
2022-09-14 21:48:14 -0700

    Fix issue with word length vs letter length in the component

--- src/PasswordGenerator.js
+++ src/PasswordGenerator.js
@@ -13,7 +13,10 @@ import { TYPE } from './lib/constants';
 const PasswordGenerator = (props) => {
   const { onPasswordGenerated, requirements } = props;
   const [type, setType] = React.useState(TYPE.PASSPHRASE);
-  const [length, setLength] = React.useState(requirements?.minLength ?? 4);
+  const [letterLength, setLetterLength] = React.useState(requirements?.minLength ?? 10);
+  // Math here doesn't guarantee it will be minLength from requirements but should be good enough for demo.
+  // We should consider making genpass accept a letter length even when type is passphrase
+  const [wordLength, setWordLength] = React.useState(requirements?.minLength ? Math.ceil(requirements?.minLength / 4) :  4);
   const [hasSymbols, setHasSymbols] = React.useState(
     Boolean(requirements?.minSymbol)
   )
@@ -23,9 +26,17 @@ const PasswordGenerator = (props) => {
   const [generatedPassword, setGeneratedPassword] = React.useState('');
 
   const getNewPasswordAndSetIt = async () => {
-    const password = await genpass({ type, length, symbols: hasSymbols, numbers: hasNumbers })
-      .catch((error) => { console.log(error); return ''; });
-    setGeneratedPassword(password);
+    try {
+      const password = await genpass({
+        type,
+        length: type === TYPE.PASSPHRASE ? wordLength: letterLength,
+        symbols: hasSymbols,
+        numbers: hasNumbers,
+      })
+      setGeneratedPassword(password);
+    } catch (e) {
+      console.log(e)
+    }
     onPasswordGenerated?.(password);
   };
 
@@ -35,7 +46,7 @@ const PasswordGenerator = (props) => {
 
   React.useEffect(() => {
     getNewPasswordAndSetIt();
-  }, [type, length, hasSymbols, hasNumbers]);
+  }, [type, letterLength, wordLength, hasSymbols, hasNumbers]);
 
   return (
     <form style={{ width: '340px', margin: '20px' }}>
@@ -121,14 +132,14 @@ const PasswordGenerator = (props) => {
               </Stack>
             </FormControl>
 
-            <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" alignItems="baseline">
+            {type === TYPE.PASSPHRASE && <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" alignItems="baseline">
               <FormLabel id="lengthLabel" sx={{ color: '#333', fontSize: '14px' }}> Words</FormLabel>
               <OutlinedInput
-                value={length}
+                value={wordLength}
                 type="number"
                 inputMode="numeric"
                 id="length"
-                onChange={(e) => { setLength(Number(e.target.value ?? 0)) }}
+                onChange={(e) => { setWordLength(Number(e.target.value ?? 0)) }}
                 size="small"
                 sx={{
                   backgroundColor: '#fff',
@@ -143,7 +154,31 @@ const PasswordGenerator = (props) => {
                   },
                 }}
               />
-            </Stack>
+            </Stack>}
+
+            {type !== TYPE.PASSPHRASE && <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" alignItems="baseline">
+              <FormLabel id="lengthLabel" sx={{ color: '#333', fontSize: '14px' }}>Letters</FormLabel>
+              <OutlinedInput
+                value={letterLength}
+                type="number"
+                inputMode="numeric"
+                id="length"
+                onChange={(e) => { setLetterLength(Number(e.target.value ?? 0)) }}
+                size="small"
+                sx={{
+                  backgroundColor: '#fff',
+                  fontSize: '14px',
+                  width: '50px',
+                  '& input': {
+                    padding: '6px 0',
+                    textAlign: 'center',
+                  },
+                  '& input::-webkit-inner-spin-button, & input::-webkit-outer-spin-button': {
+                    '-webkit-appearance': 'none',
+                  },
+                }}
+              />
+            </Stack>}
 
             <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" alignItems="baseline">
               <FormLabel id="numbersLabel" sx={{ color: '#333', fontSize: '14px' }}>Numbers</FormLabel>
